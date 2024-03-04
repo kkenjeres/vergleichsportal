7867778b31412bfde6ff3e26183732277a6058f5
kkenjeres <kkenjeres96@gmail.com>
2024-03-04 18:29:52 +0100

    pagination

--- src/app/components/CardsList.js
+++ src/app/components/CardsList.js
@@ -2,31 +2,98 @@
 import React, { useState, useMemo } from "react";
 import useSWR from "swr";
 import Card from "./Card";
-import SearchBar from "./SearchBar"; // Ensure the correct path to SearchBar component
+import SearchBar from "./SearchBar";
 
 const fetcher = (url) => fetch(url).then((res) => res.json());
 
 export default function CardList() {
   const [filterText, setFilterText] = useState("");
+  const [currentPage, setCurrentPage] = useState(1);
+  const itemsPerPage = 10;
+
   const { data, error } = useSWR("/api/sheets/", fetcher);
 
-  // useMemo to re-calculate the filtered data only when `data` or `filterText` changes
+  const filteredData = useMemo(() => {
+    if (!data) return [];
+    return data.filter((item) =>
+      item.name.toLowerCase().includes(filterText.toLowerCase())
+    );
+  }, [data, filterText]);
+
+  const paginatedData = useMemo(() => {
+    const startIndex = (currentPage - 1) * itemsPerPage;
+    return filteredData.slice(startIndex, startIndex + itemsPerPage);
+  }, [filteredData, currentPage]);
 
   const handleSearch = (text) => {
     setFilterText(text);
+    setCurrentPage(1);
   };
-  console.log(data);
+
+  const setPage = (page) => {
+    setCurrentPage(page);
+  };
+
   if (error) return <div>Failed to load</div>;
   if (!data) return <div>Loading...</div>;
 
+  const totalPages = Math.ceil(filteredData.length / itemsPerPage);
+
+  // Вычисляем диапазон кнопок для пагинации
+  const range = (start, end) => {
+    return Array.from({ length: end - start + 1 }, (_, i) => start + i);
+  };
+
+  // Определяем начальную и конечную страницы диапазона
+  let startPage = Math.max(1, currentPage - 1);
+  let endPage = Math.min(currentPage + 1, totalPages);
+
+  // Если текущая страница близка к краям, корректируем диапазон
+  if (currentPage === 1) {
+    endPage = Math.min(3, totalPages);
+  } else if (currentPage === totalPages) {
+    startPage = Math.max(totalPages - 2, 1);
+  }
+
   return (
-    <div className="m-auto w-full text-center mt-20">
+    <div className="m-auto text-center mt-20 w-[90%] md:w-[80%]">
       <SearchBar onSearch={handleSearch} filterText={filterText} />
-      <h2>Current promotions for “{filterText || ""}”</h2>
-      <div className="w-[80%] m-auto grid grid-cols-2 md:grid-cols-4 gap-2">
-        {data.map((item, index) => (
+      <h2 className="my-10">
+        Current promotions{filterText ? `: ${filterText}` : ""}
+      </h2>
+      <div className="m-auto grid grid-cols-2 md:grid-cols-4 gap-2">
+        {paginatedData.map((item, index) => (
           <Card key={index} data={item} />
         ))}
+      </div>
+      <div className="flex justify-center my-20">
+        <button
+          onClick={() => setPage(currentPage - 1)}
+          disabled={currentPage === 1}
+          className="mr-2 px-3 py-1 bg-blue-500 text-white rounded hover:bg-blue-600 focus:outline-none focus:bg-blue-600"
+        >
+          {"<"}
+        </button>
+        {range(startPage, endPage).map((page) => (
+          <button
+            key={page}
+            onClick={() => setPage(page)}
+            className={`mx-1 px-3 py-1 rounded focus:outline-none ${
+              currentPage === page
+                ? "bg-blue-500 text-white"
+                : "bg-gray-200 text-gray-800 hover:bg-gray-300"
+            }`}
+          >
+            {page}
+          </button>
+        ))}
+        <button
+          onClick={() => setPage(currentPage + 1)}
+          disabled={currentPage === totalPages}
+          className="ml-2 px-3 py-1 bg-blue-500 text-white rounded hover:bg-blue-600 focus:outline-none focus:bg-blue-600"
+        >
+          {">"}
+        </button>
       </div>
     </div>
   );
--- src/app/components/SearchBar.js
+++ src/app/components/SearchBar.js
@@ -8,7 +8,7 @@ const SearchBar = ({ onSearch, filterText }) => {
         placeholder="Search..."
         onChange={(e) => onSearch(e.target.value)}
         value={filterText}
-        className="..."
+        className="w-full p-3 text-sm border-2 border-gray-300 rounded-lg focus:outline-none focus:border-blue-500 focus:ring-1 focus:ring-blue-500 transition-all ease-in-out duration-300 shadow-sm placeholder-gray-400"
       />
     </div>
   );
